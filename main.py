6e0e48400586e30867df8fffa1a0917f2949c452
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-23 15:18:16 +0200

    feat: authentication with spotify

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
-from bs4 import BeautifulSoup
+import os
 import re
+import spotipy
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from spotipy.oauth2 import SpotifyOAuth
 
 URL = "https://www.billboard.com/charts/hot-100/"
 rex = re.compile("(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])")
@@ -17,6 +21,18 @@ while not correct_input:
         all_songs = soup.find_all(name="span", class_="chart-element__information__song")
         song_titles = [song.getText() for song in all_songs]
 
-        print(song_titles)
+        sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                scope="playlist-modify-private",
+                redirect_uri="http://example.com",
+                client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+                client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+                show_dialog=True,
+                cache_path="token.txt",
+            )
+        )
+
+        user_id = sp.current_user()["id"]
+        print(user_id)
 
         correct_input = True
