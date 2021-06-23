a8bb4e0fcf9c7b2d688bdc455b8f45a79e5d2ae6
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-23 15:42:00 +0200

    feat: create a new private playlist in Spotify and add songs to it

--- main.py
+++ main.py
@@ -51,4 +51,11 @@ while not correct_input:
         except IndexError:
             print(f"{song} doesn't exist in Spotify. Skipped.")
 
+        # Creating a new private playlist in Spotify
+        playlist = sp.user_playlist_create(user=user_id, name=f"{historical_date} Billboard 100", public=False)
+        # print(playlist)
+
+        # Adding songs found into the new playlist
+        sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+
         correct_input = True
